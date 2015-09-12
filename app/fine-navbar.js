fdf61b89c6df3f3c16897adacd82df5f6de9de51
Roel Franken <roelfranken@gmail.com>
2015-09-12 12:49:41 +0200

    Autorisatie obv JSON-file

--- app/fine-navbar.js
+++ app/fine-navbar.js
@@ -2,6 +2,10 @@
 
 // Declare app level module which depends on views, and components
 angular.module('fine.navbar', ['ui.bootstrap','ui.router','ui.navbar'])
+
+    .constant('RESOURCE_URL','http://localhost:3001/')
+    .constant('MAX_FETCH_SIZE',50)
+
     .config( function($stateProvider, $urlRouterProvider) {
         // Voor een URL die niet gematched kon worden, ga naar /homepage
         $urlRouterProvider.otherwise("/home")
@@ -44,7 +48,13 @@ angular.module('fine.navbar', ['ui.bootstrap','ui.router','ui.navbar'])
                 templateUrl: "partials/creditInvoice.html"
             })
             .state('meterReading', {
-                url: "/meterReading"
+                url: "/meterReading",
+                templateUrl: "partials/meterReading.html"
+            })
+            // Beheer:
+            .state('aanmakenGebruiker', {
+                url: "/aanmakenGebruiker",
+                templateUrl: "partials/aanmakenGebruiker.html"
             })
             // Dit is de default
             .state('otherwise', {
@@ -53,129 +63,157 @@ angular.module('fine.navbar', ['ui.bootstrap','ui.router','ui.navbar'])
             })
     })
 
-    .controller('NavigationController', function($scope) {
-        // Definieer de trees onder het hoofdemenu. Dit zijn er op dit moment dus de volgende:
-        // - FINE CRM    : treeFineCrm
-        // - FINE Billing: treeFineBilling
-        // - Beheer      : treeBeheer
-        // - Mijn Account: treeMijnAccount
-        //
-        // Deze zijn afhankelijk van de rechten van de gebruiker die het menu oproept.
-        // Onderstaand zou dus uit een database met rechten gehaald kunnen/moeten worden.
-        $scope.treeFineCrm = [
-            {
-                name: "Aansluitingen",
-                link: "connections"
+    .service('rolesFromFileService', function ($http) {
+            this.getData = function () {
+                return $http.get('fineroles.json');
             }
-            ,
-            {
-                name: "Contracten",
-                link: "#",
-                subtree: [{
-                    name: "Maak een contract",
-                    link: "Contract",
-                    subtree: [{ name: "Gewoon Contract",
-                                link: "makeContract"
-                              },
-                              { name: "Verzamel Contract",
-                                link: "makeCollectiveContract"
-                              }
-                    ]
-                }
-                ,
+        })
+
+    .controller('NavigationController', function($scope,rolesFromFileService) {
+
+        $scope.readingFromDatabase=true;
+        rolesFromFileService.getData()
+            .then(
+            function(result){
+                $scope.userRoles=result;
+                $scope.readingFromDatabase=false;
+                console.log('callback');
+                console.log(result);
+
+                console.log('ik ga door')
+                // Definieer de trees onder het hoofdemenu. Dit zijn er op dit moment dus de volgende:
+                // - FINE CRM    : treeFineCrm
+                // - FINE Billing: treeFineBilling
+                // - Beheer      : treeBeheer
+                // - Mijn Account: treeMijnAccount
+                //
+                // Deze zijn afhankelijk van de rechten van de gebruiker die het menu oproept.
+                // Onderstaand zou dus uit een database met rechten gehaald kunnen/moeten worden.
+                $scope.treeFineCrm = [
                     {
-                    name: "Zoek Contract",
-                    link: "searchContract"
-                }]
-            }
-            ,
-            {
-                name: "divider",
-                link: "#"
-            }
-            ,
-            {
-                name: "Facturen",
-                link: "#",
-                subtree: [
-                {
-                    name: "Factuur bekijken",
-                    link: "viewInvoice"
-                }
-                ,
-                {
-                    name: "Factuur crediteren",
-                    link: "creditInvoice"
-                }
-                ,
-                {
-                    name: "Factuur Run",
-                    link: "invoiceRun"
-                }
-                ]
-            }
-            ,
-            {
-                name: "divider",
-                link: "#"
-            }
-            ,
-            {
-                name: "Meterstand Invoeren",
-                link: "meterReading"
-            }
-        ];
+                        name: "Aansluitingen",
+                        link: "connections",
+                        roles: ['PPB_BEHEER', 'PPB_FINANCIEEL']
+                    }
+                    ,
+                    {
+                        name: "Contracten",
+                        link: "#",
+                        subtree: [{
+                            name: "Maak een contract",
+                            link: "Contract",
+                            subtree: [{ name: "Gewoon Contract",
+                                        link: "makeContract"
+                                      },
+                                      { name: "Verzamel Contract",
+                                        link: "makeCollectiveContract"
+                                      }
+                            ]
+                        }
+                        ,
+                            {
+                            name: "Zoek Contract",
+                            link: "searchContract"
+                        }]
+                    }
+                    ,
+                    {
+                        name: "divider",
+                        link: "#"
+                    }
+                    ,
+                    {
+                        name: "Facturen",
+                        link: "#",
+                        subtree: [
+                        {
+                            name: "Factuur bekijken",
+                            link: "viewInvoice"
+                        }
+                        ,
+                        {
+                            name: "Factuur crediteren",
+                            link: "creditInvoice"
+                        }
+                        ,
+                        {
+                            name: "Factuur Run",
+                            link: "invoiceRun"
+                        }
+                        ]
+                    }
+                    ,
+                    {
+                        name: "divider",
+                        link: "#"
+                    }
+                    ,
+                    {
+                        name: "Meterstand Invoeren",
+                        link: "meterReading"
+                    }
+                ];
 
-        $scope.treeFineBilling = [
-            {
-                name: "Menukeuze 1",
-                link: "fineBillingMenuChoice1"
-            }
-            ,
-            {
-                name: "Menukeuze 2",
-                link: "fineBillingMenuChoice2"
-            }
-            ,
-            {
-                name: "Menukeuze 3",
-                link: "fineBillingMenuChoice3"
-            }
-        ];
+                $scope.treeFineBilling = [
+                    {
+                        name: "Menukeuze 1",
+                        link: "fineBillingMenuChoice1"
+                    }
+                    ,
+                    {
+                        name: "Menukeuze 2",
+                        link: "fineBillingMenuChoice2"
+                    }
+                    ,
+                    {
+                        name: "Menukeuze 3",
+                        link: "fineBillingMenuChoice3"
+                    }
+                ];
 
-        $scope.treeBeheer = [
-            {
-                name: "Menukeuze 1",
-                link: "beheerMenuChoice1"
-            }
-            ,
-            {
-                name: "Menukeuze 2",
-                link: "beheerMenuChoice2"
-            }
-            ,
-            {
-                name: "Menukeuze 3",
-                link: "beheerMenuChoice3"
-            }
-        ];
+                $scope.treeBeheer = [
+                    {
+                        name: "Aanmaken Gebruiker",
+                        link: "aanmakenGebruiker",
+                        roles: ['PPB_BEHEER', 'PPB_FINANCIEEL']
+                    }
+                    ,
+                    {
+                        name: "Menukeuze 2",
+                        link: "beheerMenuChoice2"
+                    }
+                    ,
+                    {
+                        name: "Menukeuze 3",
+                        link: "beheerMenuChoice3",
+                        roles: ['PPB_BEHEER', 'PPB_FINANCIEEL']
+                    }
+                ];
 
-        $scope.treeMijnAccount = [
-            {
-                name: "Wachtwoord Veranderen",
-                link: "changePasswordChoice1"
+                $scope.treeMijnAccount = [
+                    {
+                        name: "Wachtwoord Veranderen",
+                        link: "changePasswordChoice1"
+                    }
+                    ,
+                    {
+                        name: "Instellingen",
+                        link: "myAccountMenuChoice2"
+                    }
+                    ,
+                    {
+                        name: "Menukeuze 3",
+                        link: "myAccountMenuChoice2"
+                    }
+                ];
+
             }
-            ,
-            {
-                name: "Instellingen",
-                link: "myAccountMenuChoice2"
+            // Vervolge callback:
+            ,   function(result) {
+                console.log('Fout' + result.statusText)
+                $scope.readingFromDatabase=false;
             }
-            ,
-            {
-                name: "Menukeuze 3",
-                link: "myAccountMenuChoice2"
-            }
-        ];
+        )
 
     })
 
--- app/ui-navbar.js
+++ app/ui-navbar.js
@@ -15,11 +15,27 @@ angular.module('ui.navbar', ['ui.bootstrap', 'template/navbar-ul.html', 'templat
         return {
             restrict: 'E',
             replace: true,
-            scope: {
-                leaf: '='
-            },
             templateUrl: 'template/navbar-li.html',
             link: function (scope, element, attrs) {
+                scope.leaf.isAuthorized = false
+                //var rollenVanGebruiker = ["PPB_BEHEER", "ONZIN", "NOGEEN"];
+                // Kijk of er een match is tussen de rollen van de gebruiker en de rolle van de menu-optie:
+                var rollenVanGebruiker = scope.$parent.$parent.$parent.userRoles
+                if (angular.isArray(scope.leaf.roles)) {
+                    // Kijk of er een match is tussen de rollen van de gebruiker en die van de menu-optie:
+                    angular.forEach(rollenVanGebruiker.data, function(rolGebruiker) {
+                        angular.forEach(scope.leaf.roles, function(rolMenuOptie) {
+                            if (!scope.leaf.isAuthorized) {
+                                if (rolGebruiker === rolMenuOptie) {
+                                    scope.leaf.isAuthorized = true;
+                                }
+                            }
+                        });
+                    });
+                }
+                if (!scope.leaf.isAuthorized) {
+                    scope.leaf.class='not-authorized'
+                }
                 if (angular.isArray(scope.leaf.subtree)) {
                     element.append('<tree tree=\"leaf.subtree\"></tree>');
                     var parent = element.parent();
@@ -45,7 +61,7 @@ angular.module('ui.navbar', ['ui.bootstrap', 'template/navbar-ul.html', 'templat
 angular.module("template/navbar-li.html", []).run(["$templateCache", function($templateCache) {
     $templateCache.put("template/navbar-li.html",
         "<li ng-class=\"{divider: leaf.name == 'divider'}\">\n" +
-        "    <a ui-sref=\"{{leaf.link}}\" ng-if=\"leaf.name !== 'divider'\">{{leaf.name}}</a>\n" +
+        "    <a ui-sref=\"{{leaf.link}}\" class=\"{{leaf.class}}\" ng-if=\"leaf.name !== 'divider'\">{{leaf.name}}</a>\n" +
         "</li>");
 }]);
 
